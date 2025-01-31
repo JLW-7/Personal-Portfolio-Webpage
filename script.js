69c5604e82015c4d1fae02b66b5a548d7dd80391
July Wu <toothless2012.pp@gmail.com>
2025-01-31 20:05:53 +0800

    add image slide in animation logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function isInView(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  }
 
+window.addEventListener('scroll', () => {
+    const image = document.querySelector('.about-me-image img.slide-in-image');
+    
+    if (isInView(image)) {
+      image.classList.add('visible');
+    }
+});
+  
